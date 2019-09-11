dd6019cac03dc338fd2e21494b26a6ae392c9ac0
Santeri <santeri.j.kaariainen@gmail.com>
2019-09-11 19:29:35 +0300

    fixed

--- start_server_with_updates.sh
+++ start_server_with_updates.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 docker run \
-    --restart unless-stopped
     -p 8856:80 \
     -e UPDATES=enabled \
     -v openstreetmap-data:/var/lib/postgresql/10/main \
     -d overv/openstreetmap-tile-server \
-    run
+    run --restart unless-stopped
